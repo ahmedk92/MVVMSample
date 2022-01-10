1479d627a6cbdb1adcd840b5d32d8dbf4000b85b
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2022-01-10 14:36:43 +0200

    Initial Commit

---
+++ MVVMRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMRxSwift
+//
+//  Created by Ahmed Khalaf on 10/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVMRxSwiftUITests/MVVMRxSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVMRxSwiftUITestsLaunchTests.swift
+//  MVVMRxSwiftUITests
+//
+//  Created by Ahmed Khalaf on 10/01/2022.
+//
+
+import XCTest
+
+class MVVMRxSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
